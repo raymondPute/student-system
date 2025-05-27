1808e6bae03804253dec8abeb5bae51d05a280d1
Brandie1 <brandinamadola@gmail.com>
2025-05-27 16:48:50 +0200

    Staff commit

--- src/main/java/com/mycompany/assign/Staff.java
+++ src/main/java/com/mycompany/assign/Staff.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.assign;
 
-/**
- *
- * @author Lila Rose
- */
-public class Staff {
-    
+public class Staff extends Person {
+    private String staffId;
+    private String department;
+
+    public Staff(String name, int age, String gender, String staffId, String department) {
+        super(name, age, gender);
+        this.staffId = staffId;
+        this.department = department;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Staff ID: " + staffId);
+        System.out.println("Department: " + department);
+    }
 }
